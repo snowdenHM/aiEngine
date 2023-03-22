954eb272ff38d5aabb9076c458c3084ddfb9dd4c
harsh <harsh220902@gmail.com>
2023-03-22 20:48:06 +0530

    gitignore added

--- aiEngine/settings.py
+++ aiEngine/settings.py
@@ -12,8 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
+
 env = environ.Env()
 environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
